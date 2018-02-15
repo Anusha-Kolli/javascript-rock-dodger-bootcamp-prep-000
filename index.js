df14d83d65202887614af4d9f21d7d529d7f5f4e
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-02-15 23:19:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -198,7 +198,7 @@ function positionToInteger(p) {
 function start() {
   window.addEventListener('keydown', moveDodger)
 
-  START.style.display = 'none'
+  START.style.display = 'none';
 
   gameInterval = setInterval(function() {
     createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
