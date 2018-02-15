d9626f28cc51e5eab16cd9ec753919dc83391c29
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2018-02-15 23:14:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -143,6 +143,15 @@ function moveDodger(e) {
 }
 
 function moveDodgerLeft() {
+  if (e.which === LEFT_ARROW) {
+      e.preventDefault();
+       e.stopPropagation();
+      moveDodgerLeft();
+     }else if (e.which === RIGHT_ARROW) {
+       e.preventDefault();
+      e.stopPropagation();
+      moveDodgerRight();
+    }
   // implement me!
   /**
    * This function should move DODGER to the left
